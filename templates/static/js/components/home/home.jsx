e17b02672d59833a072b67642d5caae8ed2e963b
Eric Mockler <emockler@ucsc.edu>
2019-06-08 23:06:40 -0700

    fixed SCSScompilation

--- templates/static/js/components/home/home.jsx
+++ templates/static/js/components/home/home.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import styles from './home.sass'
 
 export default class Home extends Component {
   render() {
    return (
-    <div className={styles.home}>
+    <div className="home">
      <h1>Hello React!</h1>
     </div>
    )

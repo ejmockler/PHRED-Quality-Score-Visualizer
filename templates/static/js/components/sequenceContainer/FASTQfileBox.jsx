44c49cde750ffb7fa37548115dde92e226154338
Eric Mockler <emockler@ucsc.edu>
2019-06-10 22:14:10 -0700

    set FASTQ limitation on file upload

--- templates/static/js/components/sequenceContainer/FASTQfileBox.jsx
+++ templates/static/js/components/sequenceContainer/FASTQfileBox.jsx
@@ -1,25 +1,38 @@
 import React, {useCallback} from 'react'
 import {useDropzone} from 'react-dropzone'
+import request from 'superagent'
 
 export default function FASTQfileBox() {
- const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
-  
-  const files = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
+ const {acceptedFiles, getRootProps, getInputProps, isDragActive} = useDropzone({
+  accept: '.fq,.fastq',  
+  onDrop: acceptedFiles => {
+   const req = request.post('/upload')
+   acceptedFiles.forEach(file => {
+    req.attach(file.name, file)
+   })
+   req.end(callback)
+  }})
+ const files = acceptedFiles.map(file => (
+  <li key={file.path}>
+   {file.path} - {file.size} bytes
+  </li>
+ ));
 
-  return (
-    <section className='FASTQfileBox'>
-      <div {...getRootProps({className: 'FASTQfileBox_border'})}>
-        <input {...getInputProps()} />
-        <p>Drop FASTQ files here, or click to select files</p>
-      </div>
-      <aside className='FASTQfileBox_list'>
-        <h4>Files</h4>
-        <ul>{files}</ul>
-      </aside>
-    </section>
-  );
+ return (
+  <section className='FASTQfileBox'>
+   <div {...getRootProps({className: 'FASTQfileBox_border'})}>
+    <input {...getInputProps()} />
+    {
+      isDragActive ? (
+      <p>Drop FASTQ here...</p>) : (
+      <p>Drop FASTQ files here, or click to select files</p>)
+    }
+    <em>(Only .fq, .fastq files accepted)</em>)
+   </div>
+   <aside className='FASTQfileBox_list'>
+    <h4>Files</h4>
+    <ul>{files}</ul>
+   </aside>
+  </section>
+ );
 }

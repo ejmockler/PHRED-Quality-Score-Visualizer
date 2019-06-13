deeeeddb7eb2ff6d3bcc4dcb90dcbeaf734cb5db
Eric Mockler <emockler@ucsc.edu>
2019-06-13 06:33:27 -0700

    ready to present

--- templates/static/js/components/chart/QualityScoreZoomline.jsx
+++ templates/static/js/components/chart/QualityScoreZoomline.jsx
@@ -19,7 +19,7 @@ import ExcelExport from 'fusioncharts/features/excelexport'
 // Step 7 - Adding the chart, theme and ExcelExport as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Zoomline, CandyTheme, ExcelExport)
 
-// Step 8 - Creating the JSON object to store the chart configurations
+// Creating the JSON object to store the chart configurations
 const chartConfigs = {
     type: 'zoomline',// The chart type
     width: '100%', // Width of the chart
@@ -33,6 +33,7 @@ const chartConfigs = {
             "subcaption": "PHRED-33",
             "yaxisname": "PHRED Score",
             "xaxisname": "Base Position",
+            "labeldisplay": "auto",
             "yaxisminValue": "0",
             "yaxismaxValue": "93",
             "pixelsPerPoint": "0",
@@ -58,13 +59,13 @@ const chartConfigs = {
         },
         "categories": [
             {
-                "category": "1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24"
+                "category": "1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|"
             },
         ],
         "dataset": [
             {
                 "seriesname": "@HWI-EAS91_1_30788AAXX:1:1:1761:343",
-                "data": "60|30|45|33|67|45|27|43|70|55|60|40|67|78|64|55|62|70|53|39|42|67|70|56"
+                "data": "26|47|57|41|64|59|42|42|49|37|66|59|21|24|47|47|50|34|26|62|53|22|58|20|23|28|35|53|42|26|57|28|61|58|31|64|47|52|59|36|58|48|29|68|20|49|48|63|27|68|33|62|62|50|55|49|53|19|39|58|19|61|34|64|38|41|61|66|49|20|25|27|"
             },
         ]
     }
@@ -76,7 +77,6 @@ var centerStyle = {
 
 // Step 9 - Creating the DOM element to pass the react-fusioncharts component
 export default class QualityScoreZoomline extends Component {
-
  render() {
   return (
    <div style={centerStyle}>
